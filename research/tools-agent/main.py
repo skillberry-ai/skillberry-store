58332d0101949a446cdea6103d5b4da880200a8d
Eran Raichstein <eran.raichstein@gmail.com>
2025-02-10 09:54:43 -0600

    add support for observability

--- research/tools-agent/main.py
+++ research/tools-agent/main.py
@@ -7,7 +7,10 @@ from langchain.callbacks.tracers import ConsoleCallbackHandler
 from chat_api_server import chat_api_server
 from llm.common import llm, check_llm_communication
 from tools_agentic_graph import define_tools_agentic_graph
+from agent_analytics.instrumentation import agent_analytics_sdk 
 
+
+# Initialize logger
 logger = logging.getLogger(__name__)
 
 debug = False
@@ -26,6 +29,16 @@ else:
     set_debug(False)
     set_verbose(False)
     invoke_config = None
+
+from agent_analytics.instrumentation.configs import OTLPCollectorConfig
+
+# Initialize logging with agent_analytics_sdk
+agent_analytics_sdk.initialize_logging(
+    tracer_type=agent_analytics_sdk.SUPPORTED_TRACER_TYPES.REMOTE,
+    config=OTLPCollectorConfig(endpoint="http://localhost:4318/v1/traces"),
+    # logs_dir_path="/tmp/",
+    # log_filename="tools-agent",
+)
 
 
 def main():
