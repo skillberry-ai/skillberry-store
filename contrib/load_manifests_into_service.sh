377d3d95b5999589229aff5350641152463b26f3
Avi Weit <weit@il.ibm.com>
2025-03-06 15:09:32 +0200

    Simplify script.

--- contrib/load_manifests_into_service.sh
+++ contrib/load_manifests_into_service.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+# define the base path
+BASE_PATH=$(dirname "$0")
 
-SLEEP_TIME=5
+SLEEP_TIME=1
 
 BLUEBERRY_TOOLS_SERVICE_ROOT=~/Blueberry-tools-service
 
@@ -8,16 +9,16 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 file=$BLUEBERRY_TOOLS_SERVICE_ROOT/contrib/tools/files/functions-module.py
-cd $BLUEBERRY_TOOLS_SERVICE_ROOT
 
 echo "${bold}Creating the manifests...${normal}"
-values=(GetYear GetQuarter GetCurrencySymbol ParseDealSize GetTime add_two_number nth_prime)
-for value in "\${values[@]}"; do
-    python client/mft_ds.py ${file} ${value} > manifest-${value}.json
+declare -a values=("GetYear" "GetQuarter" "GetCurrencySymbol" "ParseDealSize" "GetTime" "add_two_numbers" "nth_prime")
+for value in "${values[@]}"; do
+    #echo "$value"
+    python ${BASE_PATH}/../client/mft_ds.py ${file} ${value} > manifest-${value}.json
 
     sleep $SLEEP_TIME
 
-    echo "${bold}Adding GetYear manifest...${normal}"
+    echo "${bold}Adding $value manifest...${normal}"
     file_manifest="./manifest-${value}.json"
     manifest=$(cat "$file_manifest")
     file_manifest_url=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$manifest'''))")
