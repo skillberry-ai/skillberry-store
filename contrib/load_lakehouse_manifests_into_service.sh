fc2ff19b014a65a9e162a76fe17f1609e24d4f6b
Avi Weit <weit@il.ibm.com>
2025-03-09 15:59:14 +0200

    Update script.

--- contrib/load_lakehouse_manifests_into_service.sh
+++ contrib/load_lakehouse_manifests_into_service.sh
@@ -15,7 +15,8 @@
 # Running this script:
 #
 # Set GENAI_PROJECT_ROOT and GENAI_TRANSFORMATION_FILE accordingly
-#
+
+
 # define the base path
 BASE_PATH=$(dirname "$0")
 
@@ -24,11 +25,11 @@ SLEEP_TIME=1
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-GENAI_PROJECT_ROOT="~/genai-lakehouse-mapping"
+GENAI_PROJECT_ROOT="/home/ubuntu/genai-lakehouse-mapping"
 GENAI_TRANSFORMATION_FILE=${GENAI_PROJECT_ROOT}/transformations/client-win-functions.py
 
 echo "${bold}Creating the manifests...${normal}"
-declare -a values=("GetYear" "GetQuarter" "GetCurrencySymbol" "ParseDealSize" "GetTime" "add_two_numbers")
+declare -a values=("GetYear" "GetQuarter" "GetCurrencySymbol" "ParseDealSize")
 for value in "${values[@]}"; do
     echo "${bold}Generating manifest: '$value' file...${normal}"
     python ${BASE_PATH}/../client/mft_ds.py ${GENAI_TRANSFORMATION_FILE} ${value} > manifest-${value}.json
@@ -38,7 +39,11 @@ for value in "${values[@]}"; do
     file_manifest="./manifest-${value}.json"
     manifest=$(cat "$file_manifest")
     file_manifest_url=$(python3 -c "import urllib.parse; print(urllib.parse.quote('''$manifest'''))")
-    curl -X POST -H 'accept: application/json' -H 'Content-Type: multipart/form-data' -F "file=@$GENAI_TRANSFORMATION_FILE" "http://localhost:8000/manifests/add?file_manifest=${file_manifest_url}" | jq .
+    curl -X POST \
+        -H 'accept: application/json' \
+        -H 'Content-Type: multipart/form-data' \
+        -F "file=@$GENAI_TRANSFORMATION_FILE" \
+        "http://localhost:8000/manifests/add?file_manifest=${file_manifest_url}" | jq .
 
     echo "${bold}Deleting local manifest: '$value' file...${normal}"
     rm -Rf manifest-${value}.json
