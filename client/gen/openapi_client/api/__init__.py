1d3dafd8a6134d21564bb26a1d116788443b1bfb
Erez Hadad <erezh@il.ibm.com>
2025-03-18 16:45:24 +0200

    1. Added generated client code to git (client/gen) 2. Removed client generation step from README.md

---
+++ client/gen/openapi_client/api/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.descriptions_api import DescriptionsApi
+from openapi_client.api.execution_api import ExecutionApi
+from openapi_client.api.files_api import FilesApi
+from openapi_client.api.manifest_api import ManifestApi
+from openapi_client.api.metadata_api import MetadataApi
+
