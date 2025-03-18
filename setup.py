500ad0befe1e5cdb0c3a83f7e2e35b85e1ed067f
Erez Hadad <erezh@il.ibm.com>
2025-03-18 17:54:23 +0200

    1. Added CLI documentation and some rework to client/README.md 2. Fixed the blueberry-tools-service package name in top setup.py (matching Python package name conventions)

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="blueberry_tools_service",
+    name="blueberry-tools-service",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,  
