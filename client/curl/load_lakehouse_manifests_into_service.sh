3cfe1a1c58408640cadb0ab426665329360af4bf
David Breitgand <davidbreitgand@users.noreply.github.com>
2025-03-20 18:27:04 +0200

    Small fix to reflect the changes in the folder structure of the project properly and use environment variable for examples.

--- client/curl/load_lakehouse_manifests_into_service.sh
+++ client/curl/load_lakehouse_manifests_into_service.sh
@@ -13,8 +13,10 @@
 # >> git checkout 7ff12d99f4533c294a0d978c4a075adda485f02b
 #
 # Running this script:
-#
-# Set GENAI_PROJECT_ROOT and GENAI_TRANSFORMATION_FILE accordingly
+# 
+# Set EXAMPLESPATH to the root of the repo (e.g., genai-lakehouse-mapping project).
+# 
+# Change GENAI_TRANSFORMATION_FILE accoif needed.
 
 
 # define the base path
@@ -28,7 +30,18 @@ SLEEP_TIME=1
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-GENAI_PROJECT_ROOT="/home/weit/genai-lakehouse-mapping"
+echo "${bold}Checking if EXAMPLESPATH is set...${normal}"
+echo "EXAMPLESPATH: ${EXAMPLESPATH}"
+
+if [ -n "${EXAMPLESPATH}" ]; then
+    GENAI_PROJECT_ROOT=${EXAMPLESPATH}
+else
+    echo "EXAMPLESPATH is not set. Please set it to the root of the repo (e.g., genai-lakehouse-mapping project)."
+    exit 1
+fi
+
+#GENAI_PROJECT_ROOT="/home/weit/genai-lakehouse-mapping"
+GENAI_PROJECT_ROOT=${EXAMPLESPATH}
 GENAI_TRANSFORMATION_FILE=${GENAI_PROJECT_ROOT}/transformations/client-win-functions.py
 
 echo "${bold}Creating the manifests...${normal}"
@@ -37,7 +50,7 @@ for value in "${values[@]}"; do
     echo "${bold}Generating manifest: '$value' file...${normal}"
     file_manifest="${BASE_PATH}/manifest-${value}.json"
 
-    python -m client.manifest_ds ${GENAI_TRANSFORMATION_FILE} ${value} > $file_manifest
+    python -m client.utils.manifest_ds ${GENAI_TRANSFORMATION_FILE} ${value} > $file_manifest
     sleep $SLEEP_TIME
 
     echo "${bold}Adding manifest: '$value'...${normal}"
