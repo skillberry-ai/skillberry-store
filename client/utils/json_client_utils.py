7cc3422d9f757462b743933912aac8f72c78353e
AVI WEIT <WEIT@il.ibm.com>
2025-04-22 16:44:08 +0300

    Python typo. (#87)

--- client/utils/json_client_utils.py
+++ client/utils/json_client_utils.py
@@ -21,7 +21,7 @@ def python_manifest_from_json_record(json_rec: dict, module_path: str):
         dict:   the manifest 
     """
     func_name = json_rec["name"]
-    manifest = init_manifest(func_name, "Python")
+    manifest = init_manifest(func_name, "python")
     manifest["name"] = func_name
     manifest["module_name"] = os.path.basename(module_path)
     manifest["state"] = "approved"
