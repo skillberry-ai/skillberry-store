2438f8baf22df526c9e50a5ea926ebd4ef47bfe0
Eran Raichstein <eranra@il.ibm.com>
2025-04-28 17:14:21 +0300

    Adding unit and end2end tests

--- contrib/examples/github/end2end.sh
+++ contrib/examples/github/end2end.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
 # This script is used to run the end-to-end test for the GitHub integration.
 # It will use the Docker container version of BTS.
 
 # note: make sure that the current directory is in `blueberry-tools>/contrib/examples/github`
+
+# Enable debugging mode
+set -x
+
 
 # Step (1) set up environment variables to pull the github repo with the tools
 
@@ -14,31 +17,78 @@ export BTS_DESCRIPTIONS_DIRECTORY="$BTS_DIRECTORY_PATH/descriptions"
 export BTS_METADATA_DIRECTORY="$BTS_DIRECTORY_PATH/metadata"
 export BTS_MANIFEST_DIRECTORY="$BTS_DIRECTORY_PATH/manifest"
 
-export GITHUB_REPO="git@github.ibm.com:ERANRA/blueberry-tools-example.git"
+export GITHUB_REPO="git@github.ibm.com:eranra/blueberry-tools-example.git"
 export BTS_POST___INIT___COMMAND="if [ -d $BTS_DIRECTORY_BASE ]; then \
     git -C $BTS_DIRECTORY_BASE pull origin main; \
 else \
     git clone $GITHUB_REPO $BTS_DIRECTORY_BASE; \
     git -C $BTS_DIRECTORY_BASE checkout main; \
-    ### TODO Load the manifests into BTS
+    ## TODO: Load the manifests into BTS (need to consult with @avi and @david)
 fi"
 
 export BTS_POST_WRITE_FILE_COMMAND="git -C $BTS_DIRECTORY_BASE add . && git -C $BTS_DIRECTORY_BASE commit -m 'Add new tool {filename}' && git -C $BTS_DIRECTORY_BASE push origin main"
 export BTS_POST_DELETE_FILE_COMMAND="git -C $BTS_DIRECTORY_BASE add . && git -C $BTS_DIRECTORY_BASE commit -m 'Delete tool {filename}' && git -C $BTS_DIRECTORY_BASE push origin main"
+sleep 10
+read -n 1 -s -r -p "==> Press any key to continue..."
 
 # step (2) start BTS Docker container
+docker stop blueberry-tools-service 2>/dev/null # stop and remove the container if it exists
+docker rm blueberry-tools-service 2>/dev/null
 
-docker run --name blueberry-tools-service --env-file .env -d -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp -p 8000:8000 artifactory.haifa.ibm.com:5130/blueberry-tools-service:latest
+docker run --name blueberry-tools-service --env-file ../../../.env -d -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp -p 8000:8000 artifactory.haifa.ibm.com:5130/blueberry-tools-service:latest
 sleep 10
+read -n 1 -s -r -p "==> Press any key to continue..."
+
+
+# step (2.1) check that the BTS service is up and running
+echo "BTS logs:"
+docker logs blueberry-tools-service > /tmp/bts.log
+echo "-------------------------------------"
+cat /tmp/bts.log
+echo "-------------------------------------"
+sleep 10
+read -n 1 -s -r -p "==> Press any key to continue..."
+
 
 # step (3) load a tool into the BTS service (this will call the relevant hook and will push the tool to the github repo)
 ../../../client/curl/load_tools.sh GetYear
 
-# step (4) observe BTS logs
+# step (3.1) observe that the tool was loaded into the BTS service
 docker logs blueberry-tools-service > /tmp/bts.log
 echo "BTS logs:"
 echo "-------------------------------------"
 cat /tmp/bts.log
 echo "-------------------------------------"
+sleep 10
+read -n 1 -s -r -p "==> Press any key to continue..."
+
+# step (4) check the local git repo to see that the tool was added
+echo "Local git repo:"
+echo "-------------------------------------"
+ls $BTS_DIRECTORY_PATH
+echo "-------------------------------------"
+sleep 10
+read -n 1 -s -r -p "==> Press any key to continue..."
+
+# step (5) check the remote git repo to see that the tool was added
+echo "Remote git repo:"
+echo "-------------------------------------"
+git -C $BTS_DIRECTORY_BASE status
+echo "-------------------------------------"
+sleep 10
+read -n 1 -s -r -p "==> Press any key to continue..."
+
+# step (6) delete the tool from the BTS service (this will call the relevant hook and will push the tool to the github repo)
+## TODO: this is not working yet (need to consult with @avi and @david)
+
+# cleanup ( stop docker, remove the container, remove the local git repo)
+docker stop blueberry-tools-service
+docker rm blueberry-tools-service
+rm -rf $BTS_DIRECTORY_BASE
+rm -rf /tmp/bts.log
+echo "-------------------------------------"
+sleep 10
+read -n 1 -s -r -p "==> Press any key to continue..."
+echo "Done."
 
 # END of e2e test
