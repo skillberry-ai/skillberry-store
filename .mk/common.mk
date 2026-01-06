f73469d4e4868fd6475ab2ae31cc9640a20237ee
Erez Hadad <erezh@il.ibm.com>
2026-01-06 13:04:56 +0200

    Double hash for common operation documentation

--- .mk/common.mk
+++ .mk/common.mk
@@ -1,22 +1,22 @@
 ##@ Common repository operations
 
-fetch-common:	# Fetch from common repo
+fetch-common:	## Fetch from common repo
 	@echo "Fetching common content from $(SB_COMMON_REMOTE)"
 	@git fetch $(SB_COMMON_REMOTE) $(SB_COMMON_BRANCH)
 
-status-common:	# Status of local common commits compared to common HEAD
+status-common:	## Status of local common commits compared to common HEAD
 	@echo "Computing delta of common from $(SB_COMMON_REMOTE)"
 	@$(SB_COMMON_PATH)/scripts/subtree-delta.sh $(SB_COMMON_REMOTE) $(SB_COMMON_BRANCH) $(SB_COMMON_PATH)
 
-pull-common:	# Pull from common repo to local common folder
+pull-common:	## Pull from common repo to local common folder
 	@echo "Pulling common content from $(SB_COMMON_REMOTE)"
 	@git subtree pull --prefix $(SB_COMMON_PATH) $(SB_COMMON_REMOTE) $(SB_COMMON_BRANCH) -m "Pull from $(SB_COMMON_REMOTE)"
 
-push-common:	# Push local commits in common folder to common repo - NOT RECOMMENDED
+push-common:	## Push local commits in common folder to common repo - NOT RECOMMENDED
 	@echo "Pushing common update to $(SB_COMMON_REMOTE)"
 	@git subtree push --prefix $(SB_COMMON_PATH) $(SB_COMMON_REMOTE) $(SB_COMMON_BRANCH)
 
-pr-common:		# Create a PR for common repo based on local changes in common folder
+pr-common:		## Create a PR for common repo based on local changes in common folder
     @SB_COMMON_REMOTE='$(SB_COMMON_REMOTE)' \
       SB_COMMON_BRANCH='$(SB_COMMON_BRANCH)' \
       SB_COMMON_PATH='$(SB_COMMON_PATH)' \
