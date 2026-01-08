a5d46633659bd388691d7f9894017a8cf1535f2d
Erez Hadad <erezh@il.ibm.com>
2026-01-08 17:21:27 +0200

    Fixed several bugs in process recipes

--- .mk/process.mk
+++ .mk/process.mk
@@ -9,18 +9,21 @@ run: install_requirements ## Run the service (idempotent)
 	@if [ -f $(SERVICE_SENTINEL) ]; then \
 		echo "$(SERVICE_NAME) service is already running. Check the SERVICE_SENTINEL file ($(SERVICE_SENTINEL))"; \
 	else \
+		rc=0; \
 		echo "Starting $(SERVICE_NAME) Service"; \
-		$(SB_COMMON_PATH)/scripts/start-service.sh $(SERVICE_LOG) $(SERVICE_SENTINEL) python -m $(SERVICE_ENTRY_MODULE); \
+		$(SB_COMMON_PATH)/scripts/start-service.sh $(SERVICE_LOG) $(SERVICE_SENTINEL) python -m $(SERVICE_ENTRY_MODULE) || rc=$$?; \
+		echo "Service $(SERVICE_NAME) terminated with exit code: $$rc" ; \
 	fi
 
 stop: ## Stop the service (idempotent)
 	@if [ -f $(SERVICE_SENTINEL) ]; then \
-		echo "Stopping Blueberry Tools Service"; \
-		$(SB_COMMON_PATH)/scripts/stop-service.sh $(TOOLS_SERVICE_SENTINEL); \
+		echo "Stopping $(SERVICE_NAME) Service"; \
+		$(SB_COMMON_PATH)/scripts/stop-service.sh $(SERVICE_SENTINEL); \
 	else \
-		echo "$(SERVICE_NAME) service is already stopped. Missing SERVICE_SENTINEL file ($(SERVICE_SENTINEL))"
+		echo "$(SERVICE_NAME) service is already stopped. Missing SERVICE_SENTINEL file ($(SERVICE_SENTINEL))"; \
+	fi
 
-clean: stop clean_service_data	## Clean all runtime data of the stopped service
+clean: stop clean_service_data	## Clean all runtime data (stopped service)
 	@rm -f $(SERVICE_SENTINEL)
 	@rm -f $(SERVICE_LOG)
 	-rm -rf __pycache__ .pytest_cache
