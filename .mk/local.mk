5ec6280ae4cb5828ecb914bcaea68097a9e33ef8
Erez Hadad <erezh@il.ibm.com>
2026-01-15 21:05:13 +0200

    Tag component by ASSET_NAME. SERVICE_* required only for service assets.

--- .mk/local.mk
+++ .mk/local.mk
@@ -1,8 +1,12 @@
 # ----------- MANDATORY IDENTIFIERS ------------------
-SERVICE_NAME := skillberry-store
-SERVICE_PORT := 8000
-SERVICE_ENTRY_MODULE := blueberry_tools_service.main
-VERSION_LOCATION := blueberry_tools_service/fast_api/git_version.py
+# Names: No spaces, letter start, letters, digits, hyphen, underscore
+# Python path names: also no hyphen
+ASSET_NAME := skillberry-store
+VERSION_LOCATION := src/skillberry_store/fast_api/git_version.py
+# If this project is a service, define these service settings as well
+SERVICE_NAME := $(ASSET_NAME)
+SERVICE_PORT := 8000 
+SERVICE_ENTRY_MODULE := skillberry_store.main
 # ----------------------------------------------------
 
 export BTS_PORT := $(or $(shell echo $$BTS_PORT), 8000) 
