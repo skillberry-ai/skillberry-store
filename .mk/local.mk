aa3e2cce8698d3e66b9e71369e5bd71d31f901c9
Erez Hadad <erezh@il.ibm.com>
2026-02-10 16:39:16 +0200

    New version - multiple ports for service, port roles, SDK

--- .mk/local.mk
+++ .mk/local.mk
@@ -2,17 +2,17 @@
 # Names: No spaces, letter start, letters, digits, hyphen, underscore
 # Python path names: also no hyphen
 ASSET_NAME := skillberry-store
+ACRONYM := SBS
+DESC_NAME := "Skillberry Store service"
 VERSION_LOCATION := src/skillberry_store/fast_api/git_version.py
 # If this project is a service, define these service settings as well
 SERVICE_NAME := $(ASSET_NAME)
-SERVICE_PORT := 8000 
+SERVICE_PORTS := 8000 8001 8002
+SERVICE_PORT_ROLES := MAIN CONFIG UI
+SERVICE_HOST := 0.0.0.0 
 SERVICE_ENTRY_MODULE := skillberry_store.main
+SERVICE_HAS_SDK := 1
 # ----------------------------------------------------
-
-export SBS_PORT := $(or $(shell echo $$SBS_PORT), 8000) 
-export SBS_HOST := $(or $(shell echo $$SBS_HOST), 0.0.0.0)
-
-SERVICE_DOCKER_SETUP := "-e SBS_HOST=$(strip $(SBS_HOST)) -e SBS_PORT=$(strip $(SBS_PORT))"
 
 include .mk/dev.mk
 include .mk/process.mk
