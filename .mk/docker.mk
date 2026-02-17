c5156484f4a9c90fbda9157ccce38c8500b1c390
Erez Hadad <erezh@il.ibm.com>
2026-02-17 14:53:57 +0200

    1. Added skillberry base image (Speedup up to x11 in image creation) 2. Docker cleanup

--- .mk/docker.mk
+++ .mk/docker.mk
@@ -1,33 +1,40 @@
 ##@ Docker container management
 
-DOCKER_HOST ?= skillberry-1.vpc.cloud9.ibm.com:8800
+# Docker registry host
+REGISTRY_HOST ?= skillberry-1.vpc.cloud9.ibm.com:8800
 # change DOCKER_PROJECT to "skillberry" for public images
 DOCKER_PROJECT ?= skillberry-dev
-
-DOCKER_REPOSITORY_NAME = $(DOCKER_HOST)/$(DOCKER_PROJECT)
-IMAGE_NAME = $(SERVICE_NAME)
-
-DOCKER_NAME = $(DOCKER_REPOSITORY_NAME)/$(IMAGE_NAME)
-DOCKER_VERSION = $(BUILD_VERSION)
-
+# Docker repo name
+REPOSITORY_NAME = $(REGISTRY_HOST)/$(DOCKER_PROJECT)
+# Docker command
 DOCKER := docker
+
+
+# Base image for skillberry services
+
+# Root image - from which the skillberry base image is built
+ROOT_IMAGE := public.ecr.aws/docker/library/python:3.11
+
+# Base image name and tag
+BASE_IMAGE_NAME := skillberry-base
+BASE_IMAGE_FULL_NAME = $(REPOSITORY_NAME)/$(BASE_IMAGE_NAME)
+BASE_IMAGE_TAG := latest
+
+# Base image dockerfile
+BASE_DOCKER_FILE := $(SB_COMMON_PATH)/Dockerfile.base
+
+
+# Image for the current service 
+
+IMAGE_NAME = $(SERVICE_NAME)
+CNTR_NAME = $(SERVICE_NAME)
+
+IMAGE_TAG = $(BUILD_VERSION)
+FULL_IMAGE_NAME = $(REPOSITORY_NAME)/$(IMAGE_NAME)
 
 DOCKER_FILE ?= Dockerfile
 
-# Check whether docker is aliased to podman
-# It is assumed that the user is using zsh or bash and alias is defined in ~/.zshrc or ~/.bashrc
-# Check that either Docker or Podman is installed
-# Check if the user has aliased Docker to Podman in their shell configuration file
-# If the user has aliased Docker to Podman, set the DOCKER variable to podman 
-# If the user has not aliased Docker to Podman, set the DOCKER variable to docker
-docker-check:
-	@echo "Checking whether Docker or Podman is installed..."
-	@if ! command -v docker > /dev/null && ! command -v podman > /dev/null; then \
-        echo "Neither Docker nor Podman is installed. Please install Docker or Podman (or both)."; \
-        exit 1; \
-    fi
 
-	@echo "Checking for Docker or Podman aliases..."
 # Search the shell configuration file to check for aliases
 # This assumes you are using zsh or bash
 # If you are using a different shell, you may need to adjust this accordingly
@@ -60,39 +67,98 @@ endif
 # Print the value of DOCKER
 @echo "Using Docker: $(DOCKER)"
 
+# Check whether docker is aliased to podman
+# It is assumed that the user is using zsh or bash and alias is defined in ~/.zshrc or ~/.bashrc
+# Check that either Docker or Podman is installed
+# Check if the user has aliased Docker to Podman in their shell configuration file
+# If the user has aliased Docker to Podman, set the DOCKER variable to podman 
+# If the user has not aliased Docker to Podman, set the DOCKER variable to docker
+docker-check:
+	@echo "Checking whether Docker or Podman is installed..."
+	@if ! command -v docker > /dev/null && ! command -v podman > /dev/null; then \
+        echo "Neither Docker nor Podman is installed. Please install Docker or Podman (or both)."; \
+        exit 1; \
+    fi
+
+.PHONY: base-image-build 
+base-image-build: docker-check .stamps/base-image-build	## Build skillberry base image
+
+# Build base image if it does not exist
+.stamps/base-image-build: 
+	@echo "Building BASE IMAGE for $(ARCH) using $(DOCKER) version: $(shell $(DOCKER) --version)"
+	@echo "Building BASE IMAGE: $(BASE_IMAGE_FULL_NAME):$(BASE_IMAGE_TAG)"
+	@echo "Using ROOT IMAGE: $(ROOT_IMAGE)"
+	@if [ "$(DOCKER)" = "docker" ]; then \
+		DOCKER_BUILDKIT=1 $(DOCKER) buildx build \
+		--file $(BASE_DOCKER_FILE) \
+		--load \
+		--build-arg ROOT_IMAGE=$(ROOT_IMAGE) \
+		-t $(BASE_IMAGE_FULL_NAME):$(BASE_IMAGE_TAG) \
+		.; \
+		touch .stamps/base-image-build; \
+	elif [ "$(DOCKER)" = "podman" ]; then \
+		$(DOCKER) build --no-cache=true \
+		--file $(BASE_DOCKER_FILE) \
+		--load \
+		--build-arg ROOT_IMAGE=$(ROOT_IMAGE) \
+		-t $(BASE_IMAGE_FULL_NAME):$(BASE_IMAGE_TAG) \
+		.; \
+		touch .stamps/base-image-build; \
+    else \
+		echo "Unsupported Docker version: $(DOCKER)"; \
+		echo "Please use Docker or Podman"; \
+		exit 1; \
+	fi
+
+# make sure that you are login into the appropriate Docker registry with required credentials
+# before running this command
+.PHONY: base-image-push
+base-image-push: docker-check base-image-build ## Push base image into the registry
+	@echo "Pushing BASE image: $(BASE_IMAGE_FULL_NAME):$(BASE_IMAGE_TAG)"
+	$(DOCKER) push $(BASE_IMAGE_FULL_NAME):$(BASE_IMAGE_TAG)
+
+.PHONY: base-image-rm
+base-image-rm: docker-check ## Remove the local base image
+	@echo "Removing BASE image: $(BASE_IMAGE_FULL_NAME):$(BASE_IMAGE_TAG)"
+	$(DOCKER) rmi -f $(BASE_IMAGE_FULL_NAME):$(BASE_IMAGE_TAG) > /dev/null 2>&1 || true
+
 .PHONY: docker-build 
-docker-build: docker-check update-git-version .stamps/docker-build
+docker-build: docker-check update-git-version .stamps/docker-build	## Build service in docker image
 
 # We actually build a new image only if the code changed
 .stamps/docker-build: .stamps/code-scan
 	@echo "Building for $(ARCH) using $(DOCKER) version: $(shell $(DOCKER) --version)"
-	@echo "Building Docker image: $(DOCKER_NAME):$(DOCKER_VERSION)"
+	@echo "Building Docker image: $(FULL_IMAGE_NAME):$(IMAGE_TAG)"
 	@echo "Build version: $(BUILD_VERSION)"
 	@echo "Build date: $(BUILD_DATE)"
-	@echo "Building for $(ARCH) using the Docker file $(DOCKER_FILE): $(DOCKER_REPOSITORY_NAME)/$(IMAGE_NAME):$(DOCKER_VERSION)"
+	@echo "Building for $(ARCH) using the Docker file $(DOCKER_FILE): $(FULL_IMAGE_NAME):$(IMAGE_TAG)"
 	@if [ "$(DOCKER)" = "docker" ]; then \
 		DOCKER_BUILDKIT=1 $(DOCKER) buildx build \
 		--file $(DOCKER_FILE) \
 		--load \
+		--build-arg BASE_IMAGE_FULL_NAME=$(BASE_IMAGE_FULL_NAME) \
+		--build-arg BASE_IMAGE_TAG=$(BASE_IMAGE_TAG) \
 		--build-arg BUILD_VERSION=$(BUILD_VERSION) \
 		--build-arg BUILD_DATE="$(BUILD_DATE)" \
 		--build-arg SERVICE_NAME="$(SERVICE_NAME)" \
 		--build-arg SERVICE_PORTS="$(SERVICE_PORTS)" \
 		--build-arg SERVICE_ENTRY_MODULE="$(SERVICE_ENTRY_MODULE)" \
-		-t $(DOCKER_NAME):$(DOCKER_VERSION) \
-		-t $(DOCKER_NAME):latest \
+		-t $(FULL_IMAGE_NAME):$(IMAGE_TAG) \
+		-t $(FULL_IMAGE_NAME):latest \
 		.; \
 		touch .stamps/docker-build; \
 	elif [ "$(DOCKER)" = "podman" ]; then \
 		$(DOCKER) build --no-cache=true \
 		--file $(DOCKER_FILE) \
+		--build-arg BASE_IMAGE_FULL_NAME=$(BASE_IMAGE_FULL_NAME) \
+		--build-arg BASE_IMAGE_TAG=$(BASE_IMAGE_TAG) \
 		--build-arg BUILD_VERSION=$(BUILD_VERSION) \
 		--build-arg BUILD_DATE="$(BUILD_DATE)" \
 		--build-arg SERVICE_NAME="$(SERVICE_NAME)" \
 		--build-arg SERVICE_PORTS="$(SERVICE_PORTS)" \
 		--build-arg SERVICE_ENTRY_MODULE="$(SERVICE_ENTRY_MODULE)" \
-		-t $(DOCKER_NAME):$(DOCKER_VERSION) \
-		-t $(DOCKER_NAME):latest \
+		-t $(FULL_IMAGE_NAME):$(IMAGE_TAG) \
+		-t $(FULL_IMAGE_NAME):latest \
 		.; \
 		touch .stamps/docker-build; \
     else \
@@ -105,37 +171,31 @@ docker-build: docker-check update-git-version .stamps/docker-build
 # before running this command
 .PHONY: docker-push
 docker-push: docker-check docker-build ## Push docker image into the registry
-	@echo "Pushing Docker image: $(DOCKER_REPOSITORY_NAME)/$(IMAGE_NAME):$(DOCKER_VERSION)"
-	$(DOCKER) push $(DOCKER_NAME):$(DOCKER_VERSION)
-	@echo "Pushing Docker image: $(DOCKER_REPOSITORY_NAME)/$(IMAGE_NAME):latest"
-	$(DOCKER) push $(DOCKER_NAME):latest
+	@echo "Pushing Docker image: $(FULL_IMAGE_NAME):$(IMAGE_TAG)"
+	$(DOCKER) push $(FULL_IMAGE_NAME):$(IMAGE_TAG)
+	@echo "Pushing Docker image: $(FULL_IMAGE_NAME):latest"
+	$(DOCKER) push $(FULL_IMAGE_NAME):latest
 
 .PHONY: docker-run
-docker-run:  docker-check docker-build docker-stop ## Run the docker image
-	$(DOCKER) run --name $(IMAGE_NAME) --env-file .env \
+docker-run: docker-check docker-build docker-clean ## Run the docker image
+	$(DOCKER) run --name $(CNTR_NAME) --env-file .env \
 		-d \
 		--network=host \
-		$(DOCKER_NAME):$(DOCKER_VERSION)
-	@echo "Docker container started: $(IMAGE_NAME)"
-	
+		$(FULL_IMAGE_NAME):$(IMAGE_TAG)
+	@echo "Docker container started: $(CNTR_NAME)"
 
 .PHONY: docker-rm
-docker-rm: docker-check docker-stop clean ## Remove the docker container, image, and temporary files
-	@echo "Removing Docker container: $(IMAGE_NAME)"
-	$(DOCKER) rm -f $(IMAGE_NAME) > /dev/null 2>&1 || true
-	@echo "Removing Docker image: $(DOCKER_NAME):$(DOCKER_VERSION)"
-	$(DOCKER) rmi -f $(DOCKER_NAME):$(DOCKER_VERSION) > /dev/null 2>&1 || true
-	@echo "Removing Docker image: $(DOCKER_REPOSITORY_NAME)/$(IMAGE_NAME):$(DOCKER_VERSION)"
-
+docker-rm: docker-check docker-clean ## Remove the docker container, image, and temporary files
+	@echo "Removing Docker image: $(FULL_IMAGE_NAME):$(IMAGE_TAG)"
+	$(DOCKER) rmi -f $(FULL_IMAGE_NAME):$(IMAGE_TAG) > /dev/null 2>&1 || true
 
 .PHONY: docker-clean
 docker-clean: docker-check docker-stop ## Remove the docker container and temporary files, but keeping the image
-	@echo "Removing Docker container: $(IMAGE_NAME)"
-	$(DOCKER) rm -f $(IMAGE_NAME) > /dev/null 2>&1 || true
+	@echo "Removing Docker container: $(CNTR_NAME)"
+	$(DOCKER) rm -f $(CNTR_NAME) > /dev/null 2>&1 || true
 
 .PHONY: docker-stop
 docker-stop: docker-check ## Stop the docker container
-	@echo "Stopping Docker container: $(IMAGE_NAME)"
-	$(DOCKER) stop $(IMAGE_NAME) > /dev/null 2>&1 || true
-	$(DOCKER) rm $(IMAGE_NAME) > /dev/null 2>&1 || true
+	@echo "Stopping Docker container: $(CNTR_NAME)"
+	$(DOCKER) stop $(CNTR_NAME) > /dev/null 2>&1 || true
 	
