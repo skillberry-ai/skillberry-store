f8a2dad400634cf832e7d3ab72e277b53b2941a0
Kalman Meth <meth@il.ibm.com>
2026-03-30 13:16:58 +0300

    updated list of valid vector databases

--- src/skillberry_store/vdbs/identify_vdb.py
+++ src/skillberry_store/vdbs/identify_vdb.py
@@ -4,6 +4,8 @@ from enum import Enum
 
 from skillberry_store.vdbs.vector_db_interface import VectorDBInterface
 from skillberry_store.vdbs.faiss import FaissDB
+from skillberry_store.vdbs.chroma import ChromaVectorDB
+from skillberry_store.vdbs.lancedb import LanceDB
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,6 +14,8 @@ logger = logging.getLogger(__name__)
 class VectorDBType(str, Enum):
     """Supported vector database types"""
     FAISS = "faiss"
+    CHROMA = "chroma"
+    LANCEDB = "lancedb"
 
 
 def identify_vector_db(db_type: VectorDBType) -> VectorDBInterface:
@@ -26,6 +30,8 @@ def identify_vector_db(db_type: VectorDBType) -> VectorDBInterface:
     """
     db_classes = {
         VectorDBType.FAISS: FaissDB,
+        VectorDBType.CHROMA: ChromaVectorDB,
+        VectorDBType.LANCEDB: LanceDB,
     }
 
     logger.info(f"identify_vector_db, db_type = {db_type}")
