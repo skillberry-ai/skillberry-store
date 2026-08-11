1ae72c57214fb15335b3cf8c7ecf5eac5eb17cd9
Erez Hadad <erezh@il.ibm.com>
2026-08-11 17:37:03 +0300

    Made skillberry-store more K8s-compatible by removing the file watchers of UI server (vite preview)

--- .mk/process.mk
+++ .mk/process.mk
@@ -1,5 +1,10 @@
 ##@ Setup & teardown as a process
 
+# Add ui-build as a prerequisite of the common `run` target so the UI static
+# bundle exists (and is current with respect to sources and ACL config) before
+# UIManager spawns `vite preview`. The recipe lives in skillberry-common.
+run: ui-build
+
 clean-service-data: stop
 	@echo "Clean $(SERVICE_NAME) /tmp directory"
 	+rm -rf /tmp/manifest
